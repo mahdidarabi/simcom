9f9b207508b465c4353342c3087706dffd4181a9
Mahdi Darabi <mahdidarabi7@yahoo.com>
2022-01-26 18:46:03 +0330

    Add SimComUtility utility class

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Text;
+using SimCom.Utilities;
+
+Console.WriteLine("App Started...\n");
+
+SimComUtility simCom = new SimComUtility("COM4");
+
+bool result = simCom.sendSms("+989337983009", "تست");
+
+Console.WriteLine(result.ToString());
---
+++ Utilities/SimComUtility.cs
@@ -0,0 +1,74 @@
+using System.IO;
+using System.IO.Ports;
+using System.Text;
+
+namespace SimCom.Utilities;
+
+public class SimComUtility
+{
+    private string _enter = Environment.NewLine;
+
+    private SerialPort _port = new SerialPort();
+
+    public SimComUtility()
+    {
+        _port.PortName = "/dev/ttyUSB0";
+    }
+
+
+    public SimComUtility(string portName)
+    {
+        _port.PortName = portName;
+    }
+
+    public bool sendSms(string phoneNumber, string msg)
+    {
+        _port.Close();
+        _port.Open();
+
+        _port.Write("AT" + _enter);
+        Thread.Sleep(100);
+
+        _port.Write("AT+CSCS=\"HEX\"" + _enter);
+        Thread.Sleep(100);
+
+        _port.Write("AT+CSMP=49,167,0,8" + _enter);
+        Thread.Sleep(100);
+
+        _port.Write("AT+CMGF=1" + _enter);
+        Thread.Sleep(100);
+
+        _port.Write("AT+CMGS=\"" + phoneNumber + "\"" + _enter);
+        Thread.Sleep(100);
+
+        string unicodedMessage = convertToHex(msg);
+
+        _port.Write(unicodedMessage + (char)26 + _enter);
+        Thread.Sleep(100);
+
+        var response = _port.ReadExisting();
+
+        _port.Close();
+
+        bool smsSent = !response.Contains("ERROR");
+        return smsSent;
+    }
+
+    private string convertToHex(string text)
+    {
+        byte[] ba = Encoding.Unicode.GetBytes(text);
+        var hexString = BitConverter.ToString(ba);
+        string[] ha = hexString.Split("-");
+
+        for (var i = 0; i < ha.Length; i += 2)
+        {
+            var temp = ha[i];
+            ha[i] = ha[i + 1];
+            ha[i + 1] = temp;
+        }
+
+        hexString = String.Join("", ha);
+        return hexString;
+    }
+
+}
